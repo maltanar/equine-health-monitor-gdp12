a0db0258eb942ed19d0e5633002fd495feafa883
Yaman Umuroglu <maltanar@gmail.com>
2012-11-10 15:50:44 +0000

    initial commit for I2C bus driver with singleton pattern

---
+++ STK3300-TinyGecko/common/drivers/i2cbus.h
@@ -0,0 +1,40 @@
+// University of Southampton, 2012
+// EMECS Group Design Project
+
+// The I2C module provides a C++ singleton interface to the I2C bus on the
+// device
+
+#ifndef I2CBUS_H
+#define I2CBUS_H
+
+#include "em_i2c.h"
+
+class I2CBus {
+public:
+  static I2CBus& getInstance()
+  {
+    static I2CBus instance;
+    return instance;
+  }
+  
+  bool readRegister16Bit(uint16_t addr, uint8_t reg, uint16_t *val);
+  bool writeRegister16Bit(uint16_t addr, uint8_t reg, uint16_t val);
+  
+  // declare the IRQ handler as a friend function, as it needs to access
+  // the protected I2C interrupt handler function from this class
+  friend void I2C0_IRQHandler(void);
+  
+private:
+  // ------ start of singleton pattern specific section ------
+  I2CBus();  
+  I2CBus(I2CBus const&);                // do not implement
+  void operator=(I2CBus const&);        // do not implement
+  // ------ end of singleton pattern specific section --------
+
+protected:
+  I2C_TransferReturn_TypeDef m_status; 
+  
+  void handleI2CInterrupt();
+};
+
+#endif // I2CBUS_H
