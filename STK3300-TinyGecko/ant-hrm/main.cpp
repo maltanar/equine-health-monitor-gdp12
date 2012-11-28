a8ef63c7789df5239d46036443886362aaa5c9f3
Yaman Umuroglu <maltanar@gmail.com>
2012-11-28 13:49:17 +0000

    add TG project for developing ANT C++ implementation

---
+++ STK3300-TinyGecko/ant-hrm/main.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "em_usart.h"
+#include "em_chip.h"
+#include "em_leuart.h"
+#include "em_emu.h"
+#include "em_gpio.h"
+#include "uartmanager.h"
+
+extern "C" {
+void TRACE_SWOSetup();
+}
+
+void frameHandler(uint8_t *buf)
+{
+  // GPSSensor::getInstance()->sampleSensorData();
+}
+
+void rxHook(int c)
+{
+  /*if(c == '\n')
+    GPSSensor::getInstance()->sampleSensorData();*/
+}
+
+int main()
+{
+  CHIP_Init();
+  TRACE_SWOSetup();  
+  //UARTManager::getInstance()->getPort(UARTManagerPortLEUART0)->setSignalFrameHook(&frameHandler);
+  //UARTManager::getInstance()->getPort(UARTManagerPortLEUART0)->setRxHook(&rxHook);
+  
+  while(1)
+  {
+    EMU_EnterEM2(true);
+  }
+}
