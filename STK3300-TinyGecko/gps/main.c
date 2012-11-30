a4fa72c0b261f93173d003ab66e18bbe86e35246
merveoksar <merveoksar@gmail.com>
2012-11-30 20:26:01 +0000

    Unit test projects were created.

---
+++ STK3300-TinyGecko/gps/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "em_usart.h"
+#include "em_chip.h"
+#include "em_leuart.h"
+#include "retargetserial.h"
+void setupSWO(void)
+{
+  uint32_t *dwt_ctrl = (uint32_t *) 0xE0001000;
+  uint32_t *tpiu_prescaler = (uint32_t *) 0xE0040010;
+  uint32_t *tpiu_protocol = (uint32_t *) 0xE00400F0;
+
+  CMU->HFPERCLKEN0 |= CMU_HFPERCLKEN0_GPIO;
+  /* Enable Serial wire output pin */
+  GPIO->ROUTE |= GPIO_ROUTE_SWOPEN;
+#if defined(_EFM32_GIANT_FAMILY)
+  /* Set location 0 */
+  GPIO->ROUTE = (GPIO->ROUTE & ~(_GPIO_ROUTE_SWLOCATION_MASK)) | GPIO_ROUTE_SWLOCATION_LOC0;
+
+  /* Enable output on pin - GPIO Port F, Pin 2 */
+  GPIO->P[5].MODEL &= ~(_GPIO_P_MODEL_MODE2_MASK);
+  GPIO->P[5].MODEL |= GPIO_P_MODEL_MODE2_PUSHPULL;
+#else
+  /* Set location 1 */
+  GPIO->ROUTE = (GPIO->ROUTE & ~(_GPIO_ROUTE_SWLOCATION_MASK)) | GPIO_ROUTE_SWLOCATION_LOC1;
+  /* Enable output on pin */
+  GPIO->P[2].MODEH &= ~(_GPIO_P_MODEH_MODE15_MASK);
+  GPIO->P[2].MODEH |= GPIO_P_MODEH_MODE15_PUSHPULL;
+#endif
+  /* Enable debug clock AUXHFRCO */
+  CMU->OSCENCMD = CMU_OSCENCMD_AUXHFRCOEN;
+
+  while(!(CMU->STATUS & CMU_STATUS_AUXHFRCORDY));
+
+  /* Enable trace in core debug */
+  CoreDebug->DHCSR |= 1;
+  CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+
+  /* Enable PC and IRQ sampling output */
+  *dwt_ctrl = 0x400113FF;
+  /* Set TPIU prescaler to 16. */
+  *tpiu_prescaler = 0xf;
+  /* Set protocol to NRZ */
+  *tpiu_protocol = 2;
+  /* Unlock ITM and output data */
+  ITM->LAR = 0xC5ACCE55;
+  ITM->TCR = 0x10009;
+}
+
+
+int main()
+{
+  CHIP_Init();
+  setupSWO();
+  printf("Hello world! \n"); 
+  RETARGET_SerialInit();
+  int c;
+  while(1) {
+    c=RETARGET_ReadChar();
+    if(c!=-1)
+      putchar(c);
+  }
+  return 0;
+}
---
+++ pcb-monitoringdevice/ANTUnitTest/debug_output_control.h
@@ -0,0 +1,101 @@
+#include <stdio.h>
+
+// University of Southampton, 2012
+// EMECS Group Design Project
+
+// This file enables or disables the module debug outputs via printf
+// from different modules
+
+#ifndef DEBUGOUTPUTCONTROL_H
+#define DEBUGOUTPUTCONTROL_H
+
+#include <stdio.h>
+
+// ************************************************************************
+// Use the statement below to disable ALL debug outputs, takes priority
+// over sections below
+// ************************************************************************
+
+#define nDISABLE_ALL_DEBUG_OUTPUTS
+
+// ************************************************************************
+// Use the section below to enable / disable debug outputs for each module
+// Example:
+// #define ENABLE_DEBUG_OUTPUT_ALARM - to enable debug output from Alarm module
+// #define nENABLE_DEBUG_OUTPUT_ALARM - to disable debug output from Alarm md.
+// ************************************************************************
+
+#ifndef DISABLE_ALL_DEBUG_OUTPUTS
+
+#define nENABLE_DEBUG_OUTPUT_ALARM
+#define nENABLE_DEBUG_OUTPUT_ANTIF
+#define nENABLE_DEBUG_OUTPUT_UART
+#define nENABLE_DEBUG_OUTPUT_HRMRX
+#define nENABLE_DEBUG_OUTPUT_TEMP
+#define nENABLE_DEBUG_OUTPUT_I2C
+#define nENABLE_DEBUG_OUTPUT_GPS
+#define nENABLE_DEBUG_OUTPUT_ACCL
+#define nENABLE_DEBUG_OUTPUT_XBEE
+
+#endif
+
+// ************************************************************************
+// Implementations for debug functions
+// No need to make changes here, modify statements above instead
+// ************************************************************************
+
+#ifdef ENABLE_DEBUG_OUTPUT_ALARM
+#define module_debug_alarm(fmt, ...)   printf("ALARM: "fmt"\n", ##__VA_ARGS__)
+#else
+#define module_debug_alarm(fmt, ...)   
+#endif
+
+#ifdef ENABLE_DEBUG_OUTPUT_ANTIF
+#define module_debug_antif(fmt, ...)   printf("ANTIF: "fmt"\n", ##__VA_ARGS__)
+#else
+#define module_debug_antif(fmt, ...)   
+#endif
+
+#ifdef ENABLE_DEBUG_OUTPUT_UART
+#define module_debug_uart(fmt, ...)   printf("UART: "fmt"\n", ##__VA_ARGS__)
+#else
+#define module_debug_uart(fmt, ...)   
+#endif
+
+#ifdef ENABLE_DEBUG_OUTPUT_HRMRX
+#define module_debug_hrmrx(fmt, ...)   printf("HRMRX: "fmt"\n", ##__VA_ARGS__)
+#else
+#define module_debug_hrmrx(fmt, ...)   
+#endif
+
+#ifdef ENABLE_DEBUG_OUTPUT_TEMP
+#define module_debug_temp(fmt, ...)   printf("TEMP: "fmt"\n", ##__VA_ARGS__)
+#else
+#define module_debug_temp(fmt, ...)   
+#endif
+
+#ifdef ENABLE_DEBUG_OUTPUT_I2C
+#define module_debug_i2c(fmt, ...)   printf("I2C: "fmt"\n", ##__VA_ARGS__)
+#else
+#define module_debug_i2c(fmt, ...)   
+#endif
+
+#ifdef ENABLE_DEBUG_OUTPUT_GPS
+#define module_debug_gps(fmt, ...)   printf("GPS: "fmt"\n", ##__VA_ARGS__)
+#else
+#define module_debug_gps(fmt, ...)   
+#endif
+
+#ifdef ENABLE_DEBUG_OUTPUT_ACCL
+#define module_debug_accl(fmt, ...)   printf("ACCL: "fmt"\n", ##__VA_ARGS__)
+#else
+#define module_debug_accl(fmt, ...)   
+#endif
+
+#ifdef ENABLE_DEBUG_OUTPUT_XBEE
+#define module_debug_xbee(fmt, ...)   printf("XBee: "fmt"\n", ##__VA_ARGS__)
+#else
+#define module_debug_xbee(fmt, ...)
+#endif
+
+#endif // DEBUGOUTPUTCONTROL_H
