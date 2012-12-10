281a8da222bf268816b2957858a60eb7caec2d5b
Yaman Umuroglu <maltanar@gmail.com>
2012-12-10 03:54:54 +0000

    rework GPS unit test to use 4-phase power management, power off everything else before starting

--- pcb-monitoringdevice/GPSUnitTest/GPSUnitTest.cpp
+++ pcb-monitoringdevice/GPSUnitTest/GPSUnitTest.cpp
@@ -9,14 +9,50 @@
 #include "em_emu.h"
 #include "em_gpio.h"
 #include "trace.h"
-#include "rtc.h"
+#include "port_config.h"
+#include "alarmmanager.h"
 
 // include files for sensors
 #include "gpssensor.h"
+#include "anthrmsensor.h"
+#include "accelerationsensor.h"
+#include "temperaturesensor.h"
+
 
 #define	SENSOR_GPS_READ_PERIOD	4
+GPSSensor * gps;
+AlarmManager * alarmManager;
 
-Sensor * gps_Sensor;
+void allSensorsOff()
+{
+	// turn off GPS power
+	GPSSensor::configurePower();
+	GPSSensor::setPower(false, false);
+	// turn off ANT power
+	ANTHRMSensor::configurePower();
+	ANTHRMSensor::setPower(false);
+	// turn off XBee power
+	XBEE_GPIO_CONFIG();
+	XBEE_POWER(false);
+	// set accelerometer and temp sensor to sleep mode
+	TemperatureSensor::getInstance()->setSleepState(true);
+	AccelerationSensor::getInstance()->setSleepState(true);
+}
+
+bool wakeup = false;
+bool read = false;
+	
+void wakeupHandler(AlarmID id)
+{
+	wakeup = true;
+}
+
+void readHandler(AlarmID id)
+{
+	read = true;
+}
+
+
 
 /**************************************************************************//**
  * @brief  Main function
@@ -25,30 +61,55 @@ int main(void)
 {
 	TRACE_SWOSetup();
 	
-	gps_Sensor  = GPSSensor::getInstance();
+	allSensorsOff();
+	
+	AlarmManager * mgr = AlarmManager::getInstance();
+	
+	gps  = GPSSensor::getInstance();
+	gps->initialize();
+	gps->setParseOnReceive(true);
+	gps->setSleepState(true);
+		
+	mgr->pause();
+	mgr->createAlarm(5, false, &wakeupHandler);
+	AlarmID readID = mgr->createAlarm(5, false, &readHandler);
+	mgr->setAlarmTimeout(readID, 7);
+	mgr->resume();
 	
 	uint16_t size;
-	SensorMessage *msg;
-	GPSMessage *gps;
+	GPSMessage * gpsMsg;
+	SensorMessage * msg;
 	
 	while (1)
 	{
-          // uncomment the line below if the device does not wake from EM2
-          // RTC_Trigger(1000, NULL);
-          EMU_EnterEM2(true);
+		EMU_EnterEM2(true);
+		
+		if(wakeup)
+		{
+			printf("wakeup! \n");
+			wakeup = false;
+			gps->setSleepState(false);
+		}
+		
+		if(read)
+		{
+			printf("read! \n");
+			read = false;
+			gps->sampleSensorData();
+			gps->setSleepState(true);
+			msg = (SensorMessage *) gps->readSensorData(&size);
+			gpsMsg = (GPSMessage *) msg->sensorMsgArray;
+			if(gpsMsg->validPosFix)
+				printf("GPS: position fixed! \n");
+			else
+				printf("GPS: position not fixed \n");
+			
+			printf("GPS: %d %d %d, %d %d %d \n", gpsMsg->latitude.degree,
+					 gpsMsg->latitude.minute, gpsMsg->latitude.second, 
+				 	gpsMsg->longitude.degree, gpsMsg->longitude.minute, 
+				 	gpsMsg->longitude.second);
+			
 
-          printf("sample and read for gps sensor \n");
-          gps_Sensor->sampleSensorData();
-          
-          msg = (SensorMessage *) gps_Sensor->readSensorData(&size);
-            
-
-                  gps = (GPSMessage *) msg->sensorMsgArray;
-                  printf("GPS: %d %d %d, %d %d %d \n", gps->latitude.degree,
-                             gps->latitude.minute, gps->latitude.second, 
-                             gps->longitude.degree, gps->longitude.minute, 
-                             gps->longitude.second);
-	
+		}
 	}
-
 }
